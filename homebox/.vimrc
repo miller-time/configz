9d727dc97c4f1b8a8f6ae65c0aedf052bc54fd7e
Russell <miller.time.baby@gmail.com>
2010-08-27 21:41:46 -0700

    updated homebox for xmonad

--- homebox/.vimrc
+++ homebox/.vimrc
@@ -1,2 +1,5 @@
 set autoindent
 set textwidth=80
+set expandtab
+set tabstop=4
+
--- homebox/sh/go.sh
+++ homebox/sh/go.sh
@@ -3,6 +3,5 @@ pkill wpa_supplicant
 pkill dhclient
 wpa_supplicant -B -iwlan0 -c/etc/wpa_supplicant.conf
 dhclient wlan0
-/etc/init.d/apache2 restart
 mount.ntfs-fuse /dev/sda2 /media/windows
-banner hello
+figlet HELLO
